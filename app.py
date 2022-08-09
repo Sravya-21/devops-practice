3f370820af7a66900783968b24191264471c9078
Sravya-21 <sravyasreem99@gmail.com>
2022-08-10 04:09:48 +0530

    first commit

--- app.py
+++ app.py
@@ -4,6 +4,9 @@
 num1=7
 num2=10
 addition=(num1+num2)
+subtraction=(num2-num1)
 print("First number :",num1)
 print("Second number :",num2)
 print("Addition : ",addition)
+print("Subtraction : ",subtraction)
+
